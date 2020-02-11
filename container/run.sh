09ccb9b3c28c9fb65aa8948d3af80e71e52e1f04
EC2 Default User <ec2-user@ip-172-16-60-5.ec2.internal>
2020-02-11 03:17:33 +0000

    added integration with sagemaker-containers

--- container/run.sh
+++ container/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cp -arf /opt/ml/input/data/train/ /ml-agents-0.11.0/unity-volume/
-cp /ml-agents-0.11.0/config/trainer_config.yaml /ml-agents-0.11.0/unity-volume/
-mlagents-learn --docker-target-name=ml-agents-0.11.0/unity-volume/train --env=3DBall --train trainer_config.yaml
-cp -arf /ml-agents-0.11.0/unity-volume/train/models /opt/ml/model/
-cp -arf /ml-agents-0.11.0/unity-volume/train/summaries /opt/ml/model/
+echo $SM_HP_ENV_NAME
+cp -arf $SM_CHANNEL_TRAIN /ml-agents-0.11.0/unity-volume/
+cp /ml-agents-0.11.0/config/trainer_config.yaml /ml-agents-0.11.0/unity-volume/train/
+mlagents-learn --docker-target-name=ml-agents-0.11.0/unity-volume/train --env=$SM_HP_ENV_NAME --train trainer_config.yaml
+cp -arf /ml-agents-0.11.0/unity-volume/train/models $SM_MODEL_DIR
+cp -arf /ml-agents-0.11.0/unity-volume/train/summaries $SM_MODEL_DIR
