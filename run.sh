1543f4113737ba547e8cb57673d063ddfade1ab6
EC2 Default User <ec2-user@ip-172-16-60-5.ec2.internal>
2020-02-11 02:26:38 +0000

    removed old files

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp -arf /opt/ml/input/data/ /unity-volume/
-mlagents-learn --docker-target-name=unity-volume --env=3DBall --train trainer_config.yaml
-cp -arf /unity-volume/models /opt/ml/model/
