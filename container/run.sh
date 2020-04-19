81180659a79cb65283b1c59f93cd54270e955288
ricetuan <fanliang88@gmail.com>
2020-04-19 21:53:09 +0900

    update for ml agent 0.15

--- container/run.sh
+++ container/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
-echo $SM_HP_ENV_NAME
-cp -arf $SM_CHANNEL_TRAIN /ml-agents-0.11.0/unity-volume/
-cp /ml-agents-0.11.0/config/trainer_config.yaml /ml-agents-0.11.0/unity-volume/train/
-mlagents-learn --docker-target-name=ml-agents-0.11.0/unity-volume/train --env=$SM_HP_ENV_NAME --train trainer_config.yaml
-cp -arf /ml-agents-0.11.0/unity-volume/train/models $SM_MODEL_DIR
-cp -arf /ml-agents-0.11.0/unity-volume/train/summaries $SM_MODEL_DIR
+mkdir /ml-agents-0.15.1/unity-game/
+cp -arf $SM_CHANNEL_TRAIN/* /ml-agents-0.15.1/unity-game/
+cd /ml-agents-0.15.1/unity-game/
+chmod 755 /ml-agents-0.15.1/unity-game/$SM_HP_ENV_NAME
+mlagents-learn --env=/ml-agents-0.15.1/unity-game/$SM_HP_ENV_NAME --train /ml-agents-0.15.1/config/trainer_config.yaml
+cp -arf /ml-agents-0.15.1/unity-game/models $SM_MODEL_DIR
+cp -arf /ml-agents-0.15.1/unity-game/summaries $SM_MODEL_DIR
